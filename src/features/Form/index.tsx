71078f7b261be2185ddd8b2b004c6fbc60aca57c
Erlan-Bam <erlanzh.gg@gmail.com>
2024-10-14 20:37:45 +0500

    --NEW FORM

--- src/features/Form/index.tsx
+++ src/features/Form/index.tsx
@@ -1,9 +1,12 @@
 import Button from "@shared/ui/Buttons/ReactButton/index";
-import Input, { TelInput } from "@shared/ui/Input/index";
-import Paragraph from "@shared/ui/Paragraph/index";
+import { FormInput } from "@shared/ui/Input/index";
+import Icon from "react-icons";
+// import Paragraph from "@shared/ui/Paragraph/index";
 
 import { useTranslation } from "react-i18next";
 import styles from "./styles.module.scss";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { FaWhatsapp } from "react-icons/fa";
 
 export const FormComponent = () => {
   const { t, i18n } = useTranslation();
@@ -11,26 +14,41 @@ export const FormComponent = () => {
 
   return (
     <div className={styles.container}>
-      <Paragraph
-        text={t("contact-pc.paragraph")}
-        paragraphType="white"
-        margin="mt-4 text-center !w-full"
-      />
-      <Input
-        placeholder={t("contact-pc.placeholder1")}
-        textAlign="center"
-        name="name"
-        required
-        margin="mt-10"
-      />
-      <Input
-        placeholder={t("contact-pc.placeholder2")}
-        textAlign="center"
-        type={currentLanguage === "ru" ? "phone" : "email"}
-        name={currentLanguage === "ru" ? "phone" : "email"}
-        required
-        margin="mt-10"
-      />
+      <div className="flex items-end gap-4">
+        <FormInput
+          placeholder={t("contact-pc.placeholder1")}
+          textAlign="center"
+          type={currentLanguage === "ru" ? "phone" : "email"}
+          name={currentLanguage === "ru" ? "phone" : "email"}
+          required
+          margin="mt-10"
+        />
+        <span className="text-lg text-white text-end mb-2">или</span>
+        <FormInput
+          placeholder={t("contact-pc.placeholder2")}
+          textAlign="center"
+          name="name"
+          required
+          margin="mt-10"
+        />
+      </div>
+      <div className="w-full flex items-center mt-1">
+        <span className="text-sm text-[#9E9E9E]">
+          {t("contact-pc.subtitle")}
+        </span>
+        <span
+          className="text-sm text-white cursor-pointer ml-1"
+          onClick={() => window.open("https://wa.me/77772886600", "_blank")}
+        >
+          {t("contact-pc.subtitle2")}
+        </span>
+        <FaWhatsapp
+          className="text-green-500 ml-1 cursor-pointer"
+          style={{ fontSize: "0.8rem", color: "white" }}
+          onClick={() => window.open("https://wa.me/77772886600", "_blank")}
+        />
+      </div>
+
       <Button
         text={t("contact-pc.submit")}
         buttonType="outline"
